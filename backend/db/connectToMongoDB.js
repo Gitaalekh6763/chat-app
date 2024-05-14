092f8c8d89e878cf6cf3e8da1a43b0d16bcb62f9
Aalekh Prasad <aalekhprasad2002@gmail.com>
2024-05-14 10:51:20 +0530

    backend-first-part-complete

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,14 +1,12 @@
-import mongoose from 'mongoose';
-// import dotenv from "dotenv";
-// dotenv.config();
-const connectToMongoDB = async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_DB_URL);
-        console.log('Connected to MongoDB');
+import mongoose from "mongoose";
 
-    }catch(error){
-        console.error('Failed to connect to MongoDB',error.message); 
-    }
+const connectToMongoDB = async () => {
+	try {
+		await mongoose.connect(process.env.MONGO_DB_URL);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.log("Error connecting to MongoDB", error.message);
+	}
 };
 
 export default connectToMongoDB;
